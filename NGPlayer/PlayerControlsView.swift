d5d6789143589778953e834b863905d4c3dcc746
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-09-21 00:06:28 +0300

    Shrink player controls in landscape mode.

--- NGPlayer/PlayerControlsView.swift
+++ NGPlayer/PlayerControlsView.swift
@@ -13,10 +13,14 @@ struct PlayerControlsView: View {
 	let onNext: () -> Void
 	let onPrevious: () -> Void
 	
-	@Binding var isPlaying: Bool
+	@Binding
+	var isPlaying: Bool
 	
 	@Environment(\.safeAreaInsets)
 	private var safeAreaInsets
+
+	@Environment(\.verticalSizeClass)
+	private var verticalSizeClass
 	
 	var body: some View {
 		
@@ -41,7 +45,7 @@ struct PlayerControlsView: View {
 				}) {
 					Image(systemName: isPlaying ? "pause" : "play")
 						.foregroundColor(Color.primaryButtonForeground)
-						.font(.system(size: 44, weight: .thin))
+						.font(.system(size: verticalSizeClass == .compact ? 28 : 44, weight: .thin))
 				}
 				
 				Spacer()
@@ -51,11 +55,10 @@ struct PlayerControlsView: View {
 				}
 				.padding(.horizontal, 64)
 			}
-			.font(.system(size: 32, weight: .thin))
-			.padding(.vertical)
+			.font(.system(size: verticalSizeClass == .compact ? 22 : 32, weight: .thin))
+			.padding(.vertical, verticalSizeClass == .compact ? 2 : 16)
 			.padding(.bottom, safeAreaInsets.bottom)
-			.padding(.leading, safeAreaInsets.leading)
-			.padding(.trailing, safeAreaInsets.trailing)
+			.padding(.horizontal, max(safeAreaInsets.leading, safeAreaInsets.trailing))
 		}
 		.background(Color.secondaryBackground)
 	}
